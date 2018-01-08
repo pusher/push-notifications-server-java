9543bf5685884ddb88e2cff3e4ac60eef6adb604
Luka Bratos <luka@pusher.com>
2018-01-08 17:31:52 +0000

    Add better error handling

--- src/main/kotlin/PushNotifications.kt
+++ src/main/kotlin/PushNotifications.kt
@@ -1,16 +1,21 @@
 package com.pusher
 
 import com.google.gson.Gson
-import com.google.gson.JsonArray
+import org.apache.http.client.methods.CloseableHttpResponse
 import java.io.IOException
 import java.net.URISyntaxException
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClients
+import org.apache.http.impl.client.BasicResponseHandler
 
+
+
+class PusherAuthError(val errorMessage: String): RuntimeException()
+data class PublishNotificationResponse(val publishId: String)
+data class PushNotificationErrorResponse(val error: String, val description: String)
 
 class PushNotifications(private val instanceId: String, private val secretKey: String) {
-
     private val gson = Gson()
     private val interestsMaxLength = 164
     private val baseURL = "https://$instanceId.pushnotifications.pusher.com/publish_api/v1"
@@ -26,12 +31,13 @@ class PushNotifications(private val instanceId: String, private val secretKey: S
     }
 
     @Throws(IOException::class, InterruptedException::class, URISyntaxException::class)
-    fun publish(interests: List<String>, publishRequest: Map<String, Any>) {
-        this.validateInput(interests)
+    fun publish(interests: List<String>, publishRequest: Map<String, Any>): String {
+        this.validateInterests(interests)
 
         val publishRequestWithInterests = publishRequest.toMutableMap()
         publishRequestWithInterests.put("interests", interests)
 
+        System.out.println(gson.toJson(publishRequestWithInterests))
         val client = HttpClients.createDefault()
         val url = String.format("$baseURL/instances/%s/publishes", this.instanceId)
         val httpPost = HttpPost(url)
@@ -40,12 +46,32 @@ class PushNotifications(private val instanceId: String, private val secretKey: S
         httpPost.setHeader("Content-Type", "application/json")
         httpPost.setHeader("Authorization", String.format("Bearer %s", this.secretKey))
         val response = client.execute(httpPost)
-        System.out.println(response)
+
+        val statusCode = response.statusLine.statusCode
+
+        when (statusCode) {
+            401 -> pusherError(response)
+            404 -> pusherError(response)
+            in 400..499 -> pusherError(response)
+            in 500..599 -> pusherError(response)
+        }
+
+        val responseString = BasicResponseHandler().handleResponse(response)
+
+        return gson.fromJson(responseString, PublishNotificationResponse::class.java).publishId
     }
 
-    private fun validateInput(interests: List<String>) {
+    private fun pusherError(response: CloseableHttpResponse): PusherAuthError {
+        throw PusherAuthError(gson.fromJson(response.toString(), PushNotificationErrorResponse::class.java).description)
+    }
+
+    private fun validateInterests(interests: List<String>) {
         if (interests.isEmpty()) {
             throw IllegalArgumentException("Publish method expects at least one interest")
+        }
+
+        if (interests.count() == 1 && interests.first() == "") {
+            throw IllegalArgumentException("interest should not be an empty string")
         }
 
         interests.find { it.length > interestsMaxLength }?.let {
